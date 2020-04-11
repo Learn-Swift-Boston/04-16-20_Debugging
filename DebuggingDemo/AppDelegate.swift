68d69cfc23dfccb0377dd35fc4b08cf82ce995c0
Matt Dias <matt.dias6@gmail.com>
2020-04-11 13:22:06 -0400

    Initial Commit

---
+++ DebuggingDemo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DebuggingDemo
+//
+//  Created by Matthew Dias on 4/11/20.
+//  Copyright © 2020 Matthew Dias. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DebuggingDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DebuggingDemo
+//
+//  Created by Matthew Dias on 4/11/20.
+//  Copyright © 2020 Matthew Dias. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
